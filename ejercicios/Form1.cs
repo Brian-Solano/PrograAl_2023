e82a85311c5e01d6f4680bb3687000878951354e
Brian-Solano <thebrs010@gmail.com>
2023-08-08 10:34:38 -0600

    uso de las estructuras de control

--- ejercicios/Form1.cs
+++ ejercicios/Form1.cs
@@ -25,7 +25,7 @@ namespace ejercicios
         private void btncomprobar_Click(object sender, EventArgs e)
         {
             int edad = int.Parse(txtedad.Text);
-            if (edad >= 18) ;
+            if (edad >= 18) 
             MessageBox.Show("Bienvenido, eres responsable por tus acciones"); 
         }
     }
