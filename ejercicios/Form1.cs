5bc7178031eb00a3a84b44ab1df1f126b58ff90b
Brian-Solano <054223@ugb.edu.sv>
2023-08-15 10:20:12 -0600

    Realizacion de conversores

--- ejercicios/Form1.cs
+++ ejercicios/Form1.cs
@@ -17,16 +17,43 @@ namespace ejercicios
             InitializeComponent();
         }
 
-        private void lbledad_Click(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void btncomprobar_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            int edad = int.Parse(txtedad.Text);
-            if (edad >= 18) 
-            MessageBox.Show("Bienvenido, eres responsable por tus acciones"); 
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconvertirconversores_Click(object sender, EventArgs e)
+        {
+            int de = 0, a = 0;
+            double cantidad = 0, respuesta = 0;
+
+            de = cbodeconversores.SelectedIndex;
+            a = cboaconversores.SelectedIndex;
+
+            cantidad = double.Parse(txtcantidadconversores.Text);
+
+            double[] monedas = {1, 0.92,7.86, 24.66, 8.75, 36.58, 535.14, 145.47, 0.79, 83.29};
+
+            respuesta = monedas[a] / monedas[de] * cantidad;
+
+            lblrespuestasconversores.Text = "Respuesta: " + Math.Round(  respuesta, 3 );
+
+
         }
     }
 }
